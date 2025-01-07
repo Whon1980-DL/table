c4437e2f64287e4dd532aab969c255cc03fc5efb
Supitpong Wongsaroj <ewrctysf@students.codeinstitute.net>
2025-01-07 09:17:38 +0000

    Add code

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,5 @@
+// The .ready funciton ensure the whole html page is fully loaded before the scripts are run
 $(document).ready(function() {
-    
+    $("h2").addClass("underline"); //underlines all <h2> elements
+    $("ul").addClass("border"); //adds a border to all lists
 });
